2fdadb0ff16a7f157db709c4f8b08841d71d9075
dcode-al <nyainengsih83@gmail.com>
2025-05-23 21:56:23 +0700

    Add files via upload

---
+++ scrape/sfile.js
@@ -0,0 +1,14 @@
+const cheerio = require("cheerio")
+const fetch = require("node-fetch")
+
+async function sfileDl(url) {
+	let res = await fetch(url)
+	let $ = cheerio.load(await res.text())
+	let filename = $('div.w3-row-padding').find('img').attr('alt')
+	let mimetype = $('div.list').text().split(' - ')[1].split('\n')[0]
+	let filesize = $('#download').text().replace(/Download File/g, '').replace(/\(|\)/g, '').trim()
+	let download = $('#download').attr('href') + '&k=' + Math.floor(Math.random() * (15 - 10 + 1) + 10)
+	return { filename, filesize, mimetype, download }
+}
+
+module.exports = { sfileDl }
