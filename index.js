4062bcf2752e23a6d34b8e1d0d5e4ad5564d0d00
dcode-al <nyainengsih83@gmail.com>
2025-05-23 22:27:48 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1480,11 +1480,11 @@ app.get('/api/hblowjob', async (req, res) => {
   var response = await fetch(`https://api.waifu.pics/nsfw/blowjob`);
     var data = await response.json();
     var { url: result } = data;
-  const image = await getBuffer(result)
+  const image = await getBuffer(`${result}`)
       if (!image) res.json("Error!")
       await res.set("Content-Type", "image/png")
       await res.send(image)
-    });
+});
 
 app.use((err, req, res, next) => {
   console.log(err.stack);
